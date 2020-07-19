d10642004e18acbe4c4432323e494d728840a3d1
Rémi <58167960+D-Remi@users.noreply.github.com>
2020-07-19 13:20:01 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Client from "./Client";
+import ClientForm from "./ClientForm";
+
+import "./styles.css";
+
+class App extends React.Component {
+  state = {
+    clients: [
+      { id: 1, nom: "Remi D" },
+      { id: 2, nom: "Marco T" },
+      { id: 3, nom: "Jerome D" }
+    ]
+  };
+
+  handleDelete = id => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(function(client) {
+      return client.id === id;
+    });
+
+    clients.splice(index, 1);
+
+    this.setState({ clients: clients });
+  };
+
+  handleAdd = client => {
+    const clients = [...this.state.clients];
+
+    clients.push(client);
+    this.setState({ clients });
+  };
+
+  render() {
+    const title = "Liste des clients";
+    return (
+      <div>
+        <h1>{title}</h1>
+        <ul>
+          {this.state.clients.map(client => (
+            <Client details={client} onDelete={this.handleDelete} />
+          ))}
+        </ul>
+        <ClientForm onClientAdd={this.handleAdd} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
